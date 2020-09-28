eef529d71689330d29492c211c72caca92216a53
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-09-28 12:46:53 +0200

    Add test file

--- src/main/java/de/neuefische/students/AppMain.java
+++ src/main/java/de/neuefische/students/AppMain.java
@@ -5,5 +5,6 @@ public class AppMain {
     public static void main(String[] args) {
         Student student = new Student();
         System.out.println(student.getName());
+
     }
 }
---
+++ src/test/java/de/neuefische/students/StudentTest.java
@@ -0,0 +1,7 @@
+package de.neuefische.students;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+}
